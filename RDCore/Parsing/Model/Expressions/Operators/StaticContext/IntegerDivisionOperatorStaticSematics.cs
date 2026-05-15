e36ece9fa0944de9b0dac67ffe570c1eb27fe319
Mathieu Guindon <mathieu.guindon@dev.rubberduckvba.ca>
2026-05-15 12:54:12 -0400

    fix static semantic tests

--- RDCore/Parsing/Model/Expressions/Operators/StaticContext/IntegerDivisionOperatorStaticSematics.cs
+++ RDCore/Parsing/Model/Expressions/Operators/StaticContext/IntegerDivisionOperatorStaticSematics.cs
@@ -18,6 +18,22 @@ internal sealed record class IntegerDivisionOperatorStaticSematics : BinaryArith
             INumericType or VBStringType or VBFixedStringType or VBDateType
                 when rhs is IFloatingPointNumericType or IFixedPointNumericType or VBStringType or VBFixedStringType or VBDateType => VBLongType.TypeInfo,
 
+            // these *should* be covered by the above...
+            VBSingleType when rhs is VBByteType or VBBooleanType or VBIntegerType => VBLongType.TypeInfo,
+            VBByteType or VBBooleanType or VBIntegerType when rhs is VBSingleType => VBLongType.TypeInfo,
+
+            VBSingleType when rhs is VBLongType or VBLongLongType => VBLongType.TypeInfo,
+            VBLongType or VBLongLongType when rhs is VBSingleType => VBLongType.TypeInfo,
+
+            VBDoubleType or VBStringType or VBFixedStringType when rhs is IIntegralNumericType or IFloatingPointNumericType or VBStringType or VBFixedStringType => VBLongType.TypeInfo,
+            IIntegralNumericType or IFloatingPointNumericType or VBStringType or VBFixedStringType when rhs is VBDoubleType or VBStringType or VBFixedStringType => VBLongType.TypeInfo,
+
+            VBCurrencyType when rhs is INumericType or VBStringType or VBFixedStringType => VBLongType.TypeInfo,
+            INumericType or VBStringType or VBFixedStringType when rhs is (VBCurrencyType) => VBLongType.TypeInfo,
+
+            VBDateType when rhs is INumericType or VBStringType or VBFixedStringType or VBDateType => VBLongType.TypeInfo,
+            INumericType or VBStringType or VBFixedStringType or VBDateType when rhs is VBDateType => VBLongType.TypeInfo,
+
             _ => base.DetermineOperatorStaticType(lhs, rhs)
         };
     }
