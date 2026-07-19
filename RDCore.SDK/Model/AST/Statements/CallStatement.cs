ad50d61b08a0cae8698227d1dc1d841c005130d1
Mathieu Guindon <mathieu.guindon@dev.rubberduckvba.ca>
2026-07-19 18:47:13 -0400

    closes #79 - defines CallStatement executable node

--- RDCore.SDK/Model/AST/Statements/CallStatement.cs
+++ RDCore.SDK/Model/AST/Statements/CallStatement.cs
@@ -1,14 +1,15 @@
 ﻿using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using RDCore.SDK.Model.AST.Abstract;
 using System.Collections.Immutable;
 
-namespace RDCore.SDK.Model.AST.Abstract;
+namespace RDCore.SDK.Model.AST.Statements;
 
 /// <summary>
-/// A <c>BoundNode</c> representing an <em>executable statement</em>.
+/// 
 /// </summary>
 /// <param name="SemanticId">A semantic <c>Uri</c> uniquely identifying this specific node.</param>
 /// <param name="Location">The document location (<c>Uri</c>+<c>Range</c>) of the bound expression.</param>
 /// <param name="Token">The <c>string</c> <em>token</em> of the statement, e.g. <c>Open</c>, <c>Input</c>, <c>Print</c>, <c>Assert</c>, etc..</param>
 /// <param name="Inputs">The <em>inputs</em> of the executable statement; expressions evaluated immediately before the call.</param>
-public abstract record class BoundStatement(Uri SemanticId, Location Location, string Token, ImmutableArray<BoundExpression> Inputs) 
-    : BoundNode(SemanticId, Location), IExecutableNode;
+public record class CallStatement(Uri SemanticId, Location Location, string Token, ImmutableArray<BoundExpression> Inputs)
+    : BoundStatement(SemanticId, Location, Token, Inputs);
